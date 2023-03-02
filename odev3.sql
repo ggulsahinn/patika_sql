9d6ade60a16dd0a74a9f723b839d878ff2c6b191
gulsahsahin <guulssahh@gmail.com>
2023-03-02 23:18:42 +0300

    Add files via upload

---
+++ odev3.sql
@@ -0,0 +1,9 @@
+--select * from country
+--where country like 'A%a';
+--where country like '_____%n';
+
+select * from film
+--where title ilike '%t%t%t%t%';
+where title like 'C%' and length >90 and rental_rate = 2.99;
+
+
