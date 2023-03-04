a66cdbd15952d374d0aef43aa33ac2e71a85a52d
gulsahsahin <guulssahh@gmail.com>
2023-03-04 21:40:42 +0300

    Update odev4.sql

--- odev4.sql
+++ odev4.sql
@@ -1,12 +1,19 @@
---select distinct replacement_cost from film;
+/*
+1- film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+2- film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+3- film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+4- country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+5- city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter? */
 
---select count (distinct replacement_cost) from film;
+select distinct replacement_cost from film;
 
---select count(*) from film
---where title like 'T%' and rating = 'G';
+select count (distinct replacement_cost) from film;
 
---select count (*) from country
---where country like '_____';
+select count(*) from film
+where title like 'T%' and rating = 'G';
+
+select count (*) from country
+where country like '_____';
 
 select count (*) from city
 where city ilike '%r';
