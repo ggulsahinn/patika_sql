86d8f0ac4f1dc7c1914dfb60dc00aabe9371282f
gulsahsahin <guulssahh@gmail.com>
2023-03-04 21:39:23 +0300

    Update odev5.sql

--- odev5.sql
+++ odev5.sql
@@ -3,8 +3,8 @@
 2- film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
 3- customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız. */
 
---select * from film where title like '%n' order by length desc limit 5;
+select * from film where title like '%n' order by length desc limit 5;
 
---select * from film where title like '%n' order by length limit 5 offset 5;
+select * from film where title like '%n' order by length limit 5 offset 5;
 
 select * from customer where store_id = 1 order by last_name desc limit 4;
