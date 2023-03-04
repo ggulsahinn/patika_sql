bac79f63592fe0c57d1ad1b9e4bc886b61949c4d
gulsahsahin <guulssahh@gmail.com>
2023-03-04 21:42:22 +0300

    Update odev3.sql

--- odev3.sql
+++ odev3.sql
@@ -1,9 +1,15 @@
---select * from country
---where country like 'A%a';
---where country like '_____%n';
+/*
+1- country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+2- country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+3- film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+4- film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız. */
 
-select * from film
---where title ilike '%t%t%t%t%';
-where title like 'C%' and length >90 and rental_rate = 2.99;
+select * from country where country like 'A%a';
+
+select * from country where country like '_____%n';
+
+select * from film where title ilike '%t%t%t%t%';
+
+select * from film where title like 'C%' and length >90 and rental_rate = 2.99;
 
 
